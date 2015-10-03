bc61ef939416fffdbfd729e02b581b5e74e178f9
Meng-Yang Lee <crazylion2@gmail.com>
2015-10-04 01:48:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+GitToS3
+============  
+
+deploy file to s3. 
+
+Usage:
+============
+setup config.json
+deploy.rb   
+
+* -v verbose
+* --dryrun   
+*  -f  config file name
+
+S3  Bucket  Structure
+=================  
+
+/- deploy_bucket ( contain version.s3)
+/- publish_bucket ( no version.s3) 
+
+Otherwise, you can keep version.s3, and make it private.
+
---
+++ deploy.rb
@@ -0,0 +1,79 @@
+#encoding: utf-8
+require 'optparse'
+require 'json'
+require 's3'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+  opts.on("-f",  "config file") do |f|
+    options[:config] =f 
+  end
+  opts.on("--dryrun","Displays the operations that would be performed using the specified command without actually running them.")  do |v|
+      options[:dryrun] = true
+  end
+
+  opts.on("--repo","local git repo")  do |v|
+      options[:repo] = v
+  end
+  opts.on("")
+end.parse!
+
+options[:config] ||= "config.json"
+unless File.exist?(options[:config])
+    p  "no config file"
+    exit
+end
+
+
+#從s3上取得上次部署的version
+def find_version(bucket)
+    begin
+        object = bucket.objects.find("version.s3")
+    rescue
+        return "" 
+    end
+    return object.content.strip
+end
+
+config = JSON.load(open(options[:config]))
+service = S3::Service.new(:access_key_id => config["aws"]["accessKeyID"], :secret_access_key => config["aws"]["accesskey"])
+s3bucket=service.buckets.find(config["aws"]["bucket"])
+deployed_commit=find_version(s3bucket)  #上一次部署的commit
+target_commit="HEAD"
+git_repo= options[:repo] || config["repo"]
+
+if deployed_commit== ""
+    #進行全部上傳
+else
+    #進行部份上傳
+    cmd = "cd #{git_repo}  &&  git diff --name-status   #{deployed_commit}  #{target_commit}"
+    output = ` cd #{git_repo}  &&  git diff --name-status   #{deployed_commit}  #{target_commit}    `
+    output.each_line do |line|
+        action,file = line.split(" ")
+        action.strip!
+        file.strip!
+        if(action=='M' ||action=='A')
+            if options[:dryrun]==true
+                p  "(dryrun) upload "+file
+            else
+                print  "upload "+file +"\n"
+                new_object = s3bucket.objects.build(file)
+                new_object.content = open(repo_dir+"/"+file)
+                new_object.save
+            end
+        elsif action=='D'
+            if options[:dryrun] == true
+                p "(dryrun) delete "+file
+            else
+                print "delete "+file +"\n"
+                object=bucket.objects.find(file)
+                object.destroy
+            end
+        end
+    end
+end
+
